9e995c3a44e80319fbad60a81bee2c522c80ea72
Jin Chen <sunshinek31@163.com>
2017-03-30 21:58:11 +0800

    Initial Commit

---
+++ seqb/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  seqb
+//
+//  Created by 金晨 on 2017/3/30.
+//  Copyright © 2017年 sunshinek31. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
